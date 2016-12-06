ecba7321f99c7785d73c7ddd461680a90493c2b7
Irene Canuti <irene.canuti@gmail.com>
2016-12-06 19:00:46 +0000

    Refactoring directory.rb with variables and interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # print the list of students
 puts "The students of Villains Academy"
 puts "-----------"
@@ -14,6 +15,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 # count them
-print "Overall we have "
-print 11
-puts " great students."
+puts "Overall we have #{student_count} great students."
