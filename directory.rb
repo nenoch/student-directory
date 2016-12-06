091bfb2b3d34f1a57285e30a182fa26e3fb1b889
Irene Canuti <irene.canuti@gmail.com>
2016-12-06 18:56:06 +0000

    Add count of students in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# print the list of students
 puts "The students of Villains Academy"
 puts "-----------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+# count them
+print "Overall we have "
+print 11
+puts " great students."
