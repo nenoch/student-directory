cb71fddf335fdb6139d258cfa6105c64f2380dee
Irene Canuti <irene.canuti@gmail.com>
2016-12-06 21:06:17 +0000

    Add input of the user using while loop

--- directory.rb
+++ directory.rb
@@ -1,25 +1,42 @@
-# students into an array
-students = [ "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates" ]
+# students into hashes
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name:"Darth Vader", cohort: :november},
+#  {name:"Nurse Ratched", cohort: :november},
+#  {name:"Michael Corleone", cohort: :november},
+#  {name:"Alex DeLarge", cohort: :november},
+#  {name:"The Wicked Witch of the West", cohort: :november},
+#  {name:"Terminator", cohort: :november},
+#  {name:"Freddy Krueger", cohort: :november},
+#  {name:"The Joker", cohort: :november},
+#  {name:"Joffrey Baratheon", cohort: :november},
+#  {name:"Norman Bates", cohort: :november}
+# ]
+
 # define methods
+
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish just hit return twice."
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students!"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-----------"
 end
 
+# .each counts every instances [0]"Dr. Lecter" [1]:november
 def print(names)
   names.each do |name|
-  puts name
+  puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
@@ -28,6 +45,7 @@ def print_footer(names)
 end
 
 #print it out!
+students = input_students
 print_header
 print(students)
 print_footer(students)
