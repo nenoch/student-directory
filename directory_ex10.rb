ffe00a777e15389bb1f3c8dcbe4e372e256d359f
Irene Canuti <irene.canuti@gmail.com>
2016-12-08 08:44:22 +0000

    Complete exercise 10

---
+++ directory_ex10.rb
@@ -0,0 +1,36 @@
+# define methods
+
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish just hit return twice."
+  students = []
+  name = gets.delete!("\n")
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students!"
+    name = gets.delete!("\n")
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
+end
+
+# .each counts every instances [0]"Dr. Lecter" [1]:november
+def print(names)
+  names.each do |name|
+  puts "#{name[:name]} (#{name[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students."
+end
+
+#print it out!
+students = input_students
+print_header
+print(students)
+print_footer(students)
